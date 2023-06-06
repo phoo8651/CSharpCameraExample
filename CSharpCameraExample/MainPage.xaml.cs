53913a26812c172844d8015473f6f6a7f862d1a8
Rorum <phoo8651@gmail.com>
2023-06-06 16:07:35 +0900

    update

---
+++ CSharpCameraExample/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Windows.Media.Capture;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CSharpCameraExample
+{
+    public sealed partial class MainPage : Page
+    {
+		private MediaCapture mediaCapture;
+		public MainPage()
+        {
+            this.InitializeComponent();
+        }
+		private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+			mediaCapture = new MediaCapture();
+			await mediaCapture.InitializeAsync();
+
+			await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+			{
+				PreviewCaptureElement.Source = mediaCapture;
+
+				await mediaCapture.StartPreviewAsync();
+			});
+		}
+		private async void StopButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (mediaCapture != null)
+			{
+				await mediaCapture.StopPreviewAsync();
+				mediaCapture.Dispose();
+				mediaCapture = null;
+			}
+		}
+	}
+}
